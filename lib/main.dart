7f27fb03a18f05073e96ecbbddf8a73201126c20
assem9 <assem998@gmail.com>
2024-08-13 14:42:10 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:auto_updater/auto_updater.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  // Must add this line to ensure all necessary Flutter bindings are initialized.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Set the feed URL to your appcast.xml hosted on GitHub
+  //https://github.com/Assem9/updater_test/blob/main/windows_releases/appcast.xml
+  String feedURL = 'https://raw.githubusercontent.com/Assem9/updater_test/main/windows_releases/appcast.xml';
+
+  // Initialize the auto updater with the feed URL
+  await autoUpdater.setFeedURL(feedURL);
+  // Optionally, set a scheduled check interval (in seconds)
+  // For example, check every hour: 3600 seconds
+  await autoUpdater.setScheduledCheckInterval(3600);
+
+  // Manually check for updates
+  await autoUpdater.checkForUpdates();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Auto Updater Example'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () async {
+              // Manually trigger the update check when the button is pressed
+              await autoUpdater.checkForUpdates();
+            },
+            child: Text('Check for Updates'),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <auto_updater_windows/auto_updater_windows_plugin_c_api.h>
+#include <firebase_core/firebase_core_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AutoUpdaterWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AutoUpdaterWindowsPluginCApi"));
+  FirebaseCorePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
+}
